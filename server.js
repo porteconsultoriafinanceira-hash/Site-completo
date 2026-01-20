742acf749eb2a504e91321b668f4df3c4368f61b
porteconsultoriafinanceira-hash <porteconsultoriafinanceira@gmail.com>
2026-01-20 10:54:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ import dotenv from "dotenv";
 
 import checkoutRoutes from "./routes/checkout.routes.js";
 
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", checkoutRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API rodando com sucesso");
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
